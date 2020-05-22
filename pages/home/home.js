94a7a059bd5146ede8cc44cbe5225365b23fd4a0
liwei.liu <liwei.liu@mythware.com>
2020-05-22 18:13:04 +0800

    修改

---
+++ pages/home/home.js
@@ -0,0 +1,19 @@
+Page({
+  data: {
+    style: 7,
+    tabValue: 'name2',
+    contBarHeight: 0
+  },
+
+  radioChange (e) {
+    this.setData({
+      style: e.detail.value
+    })
+  },
+
+  headLoad (e) {
+    this.setData({
+      contBarHeight: e.detail.contBarHeight
+    })
+  }
+});
